8f0608aa85895cec21b67007b8c5c006abb04e99
Arnab Wahid <arnabwahid@gmail.com>
2020-12-18 14:33:25 +0600

    :seedling: glassdoor scraper
    Scraper functional.
    Selenium script functional.

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 09:35:56 2020
+
+@author: Arnab Wahid
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "C:/Users/Arnab/Documents/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('Data Scientist', 1000, False, path, 15)
